ee24522712256a1d18da13ef9a0e67a1e5236908
Dan Montiel <danmontielh@gmail.com>
2019-05-01 13:31:57 -0500

    add comments section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_052904) do
+ActiveRecord::Schema.define(version: 2019_05_01_171727) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
